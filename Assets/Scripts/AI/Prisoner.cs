b1c5f1bb891f2de804c5ac0054bfc06c1c23a687
Itai Yavin <Itai.yavin@gmail.com>
2016-01-30 17:22:48 +0100

    prisoner ground work WIP
    Tag for altar

---
+++ Assets/Scripts/AI/Prisoner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (Rigidbody2D))]
+
+public class Prisoner : MonoBehaviour {
+	private Vector3 velocity;
+	
+	public Vector3 chainPoint;
+	public float chainLength;
+	public float speed;
+	
+	bool isPanicing;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void move(){
+		transform.GetComponent<Rigidbody2D>().AddForce(velocity, ForceMode2D.Force);
+	}
+}
