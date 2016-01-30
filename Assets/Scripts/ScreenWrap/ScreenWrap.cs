9715e336842fedcc393c72e1a440dbb9db0a1e34
Kasper Honnens de Lichtenberg <kasper@honnens.dk>
2016-01-30 13:44:33 +0100

    Added ScreenWrap and screenWrap object

---
+++ Assets/Scripts/ScreenWrap/ScreenWrap.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenWrap : MonoBehaviour {
+
+    public Camera above;
+    public Camera below;
+    public Camera right;
+    public Camera left;
+
+    // Use this for initialization
+    void Start () {
+        Vector2 viewSize = GetSize();
+
+        below.transform.localPosition = new Vector2(0, viewSize.y);
+        above.transform.localPosition = new Vector2(0, -viewSize.y);
+        
+        right.transform.localPosition = new Vector2(viewSize.x,0);        
+        left.transform.localPosition = new Vector2(-viewSize.x,0);
+    }
+    
+    public static Vector2 GetSize(){
+        return new Vector2((Camera.main.orthographicSize * 2 * (16f / 9)), Camera.main.orthographicSize * 2);
+    }
+}
---
+++ Assets/Scripts/ScreenWrap/ScreenWrapObject.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenWrapObject : MonoBehaviour {
+
+    public enum ColliderType{
+        Circle,
+        Box
+        
+    }
+
+    public ColliderType colliderType = ColliderType.Circle;
+
+    private Collider2D above;
+    private Collider2D below;
+    private Collider2D right;
+    private Collider2D left;
+
+    // Use this for initialization
+    void Start () {
+        if (colliderType == ColliderType.Circle)
+        {
+            above = gameObject.AddComponent<CircleCollider2D>();
+            below = gameObject.AddComponent<CircleCollider2D>();
+            right = gameObject.AddComponent<CircleCollider2D>();
+            left = gameObject.AddComponent<CircleCollider2D>();
+        }else{
+            above = gameObject.AddComponent<BoxCollider2D>();
+            below = gameObject.AddComponent<BoxCollider2D>();
+            right = gameObject.AddComponent<BoxCollider2D>();
+            left = gameObject.AddComponent<BoxCollider2D>();
+        }
+        Vector2 viewSize = ScreenWrap.GetSize();
+        
+        below.offset = new Vector2(0, viewSize.y);
+        above.offset = new Vector2(0, -viewSize.y);
+        
+        right.offset = new Vector2(viewSize.x,0);        
+        left.offset = new Vector2(-viewSize.x,0);
+
+    }
+}
