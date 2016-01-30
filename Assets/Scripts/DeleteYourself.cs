b40d14d77787a25f440d05b2f9abdb25191d5e58
Frans Larsen <larsen.frans@gmail.com>
2016-01-30 20:56:14 +0100

    Bloodsplat

---
+++ Assets/Scripts/DeleteYourself.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeleteYourself : MonoBehaviour {
+
+	public float secondsUntilDelete = 5;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (WaitUntil (secondsUntilDelete));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator WaitUntil(float s) {
+		yield return new WaitForSeconds (s);
+
+		Destroy (gameObject);
+	}
+}
