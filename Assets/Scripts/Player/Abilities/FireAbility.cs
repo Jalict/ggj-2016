9fb8ac949c93603ba999347fe4cac4fb8a2de3a5
Rasmus Bons <rasmusbons@gmail.com>
2016-01-30 22:39:15 +0100

    Update FireAbility.cs

--- Assets/Scripts/Player/Abilities/FireAbility.cs
+++ Assets/Scripts/Player/Abilities/FireAbility.cs
@@ -21,10 +21,10 @@ public class FireAbility : IAbility {
             playerRef = GetComponent<Player>();
             playerIndexRef = playerRef.playerIndex;
             lastCast = Time.time;
-            GameObject obj = Object.Instantiate(projectile_prefab, transform.position, Quaternion.identity) as GameObject;
-            Projectile projectile = obj.GetComponent<Projectile>();
-            GameObject obj = Object.Instantiate(projectile_prefab, transform.position+new Vector3((float)Input.GetAxis("Xbox" + playerIndexRef + "_Look_X"),
+             GameObject obj = Object.Instantiate(projectile_prefab, transform.position+new Vector3((float)Input.GetAxis("Xbox" + playerIndexRef + "_Look_X"),
                 -(float)Input.GetAxis("Xbox" + playerIndexRef + "_Look_Y")), Quaternion.identity) as GameObject;
+            Projectile projectile = obj.GetComponent<Projectile>();
+            projetile.direction = new Vector2((float)Input.GetAxis("Xbox" + playerIndexRef + "_Look_X"), -(float)Input.GetAxis("Xbox" + playerIndexRef + "_Look_Y"));
             projectile.speed = projectileSpeed;
             projectile.sender = owner;
             projectile.damage = damage;
