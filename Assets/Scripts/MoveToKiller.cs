bbd3dccf99df23bfd3f17e99e36fb1e1527166da
Botched135 <rasmusbons@gmail.com>
2016-01-31 12:19:59 +0100

    Juggaernaut can jump on altar(semi) and cannot start the ritual. Normal players can only start ritual with 100 blood. The controller will shake to show when you got enough blood.

---
+++ Assets/Scripts/MoveToKiller.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToKiller : MonoBehaviour {
+    private float _t;
+    public float _spawnTime;
+    private float _distance;
+
+
+    public Player Killer;
+    public Player Prey;
+
+    public Vector3 killerPosition;
+    private Vector3 killedPosition;
+    // Use this for initialization
+    void Start () {
+        _t = 0;
+        _spawnTime = 5;
+        killedPosition = transform.position;
+        _distance = 1 / Vector3.Distance(killedPosition, killerPosition);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (_t < 0.98f)
+        {
+            transform.position = Vector3.Lerp(new Vector3(0,0,0), killerPosition, _t);
+
+            _t += Time.deltaTime / (_spawnTime / 1.6f); //NO IDEA LOL
+        }
+        else {
+            DestroyImmediate(gameObject);
+        }
+    }
+    public void InstantiateParticles(Player Killer, Player Prey)
+    {
+        killedPosition = Prey.transform.position;
+        killerPosition = Killer.transform.position;
+
+    }
+}
