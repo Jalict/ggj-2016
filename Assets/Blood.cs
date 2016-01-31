b41f59931f9a3d4c1a646dd122ce867c3a5500b7
Frans Larsen <larsen.frans@gmail.com>
2016-01-31 12:44:25 +0100

    lots of goodies ofr nicklas

---
+++ Assets/Blood.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blood : MonoBehaviour {
+	private SpriteRenderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
