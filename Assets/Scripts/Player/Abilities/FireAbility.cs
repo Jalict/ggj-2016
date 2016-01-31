5e4e6ff3388413f16dc3a96fe41f6013cb828666
Botched135 <rasmusbons@gmail.com>
2016-01-31 02:10:52 +0100

    Shoot like TowerFall

--- Assets/Scripts/Player/Abilities/FireAbility.cs
+++ Assets/Scripts/Player/Abilities/FireAbility.cs
@@ -23,10 +23,10 @@ public class FireAbility : IAbility {
             playerRef = GetComponent<Player>();
             playerIndexRef = playerRef.playerIndex;
             lastCast = Time.time;
-             GameObject obj = Object.Instantiate(projectile_prefab, transform.position+new Vector3((float)Input.GetAxis("Xbox" + playerIndexRef + "_Look_X"),
-                -(float)Input.GetAxis("Xbox" + playerIndexRef + "_Look_Y")), Quaternion.identity) as GameObject;
+             GameObject obj = Object.Instantiate(projectile_prefab, transform.position+new Vector3((float)Input.GetAxis("Xbox" + playerIndexRef + "_X_Axis_Left"),
+                -(float)Input.GetAxis("Xbox" + playerIndexRef + "_Y_Axis_Left")), Quaternion.identity) as GameObject;
             Projectile projectile = obj.GetComponent<Projectile>();
-            projectile.direction = new Vector2((float)Input.GetAxis("Xbox" + playerIndexRef + "_Look_X"), -(float)Input.GetAxis("Xbox" + playerIndexRef + "_Look_Y"));
+            projectile.direction = new Vector2((float)Input.GetAxis("Xbox" + playerIndexRef + "_X_Axis_Left"), -(float)Input.GetAxis("Xbox" + playerIndexRef + "_Y_Axis_Left"));
             projectile.speed = projectileSpeed;
             projectile.sender = owner;
             projectile.damage = damage;
